0cb07c66b8b810db520e446b5050b00844e260e9
cookieCrood <139991505+cookieCrood@users.noreply.github.com>
2025-05-13 17:12:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -19,21 +19,6 @@ client.once(Events.ClientReady, (c) => {
     console.log(`Logged in as ${c.user.tag}`);
 });
 
-client.on(Events.InteractionCreate, (interaction) => {
-
-    console.log(`Interaction: ${interaction.customId || interaction} | ran by ${interaction.user.tag} or ${interaction.user.id}`)
-
-    let command = client.commands.get(interaction.commandName);
-
-    try{
-        if(interaction.replied) return;
-        command.execute({ interaction: interaction, client: client });
-    } catch (error) {
-        console.error(error);
-    }
-
-});
-
 client.on(Events.MessageCreate, (message) => {
 
     if (message.author.bot) { return }
@@ -43,34 +28,3 @@ client.on(Events.MessageCreate, (message) => {
 })
 
 client.login(token);
-
-function getCommands(dir) {
-    let commands = new Collection();
-    const commandFiles = getFiles(dir)
-
-    for (const commandFile of commandFiles) {
-        const command = require(commandFile);
-        commands.set(command.data.name, command)
-    }
-    return commands;
-}
-
-function getFiles(dir) {
-
-    const files = fs.readdirSync(dir, {
-        withFileTypes: true
-    })
-    let commandFiles = [];
-
-    for (const file of files) {
-        if(file.isDirectory()) {
-            commandFiles = [
-                ...commandFiles,
-                ...getFiles(`${dir}/${file.name}`)
-            ]
-        } else if (file.name.endsWith(".js")) {
-            commandFiles.push(`${dir}/${file.name}`)
-        }
-    }
-    return commandFiles;
-}
